d826a6cf55ab9ea95c610903737cd65477320d43
GooseMane <yerzhan.appazov@gmail.com>
2021-09-17 15:48:27 +0600

    step 1

---
+++ Game_IT2001_2-main/Item.cpp
@@ -0,0 +1,30 @@
+//
+// Created by GooseMane on 17.09.2021.
+//
+
+#include "Item.h"
+Item::Item(string name, int atk) {
+    this->name = name;
+    this->atk = atk;
+}
+
+const string &Item::getName() const {
+    return name;
+}
+
+void Item::setName(const string &name) {
+    Item::name = name;
+}
+
+int Item::getAtk() const {
+    return atk;
+}
+
+void Item::setAtk(int atk) {
+    Item::atk = atk;
+}
+
+Item::Item() {
+    this->name = "";
+    this->atk = 0;
+}
---
+++ Game_IT2001_2-main/Item.h
@@ -0,0 +1,30 @@
+//
+// Created by GooseMane on 17.09.2021.
+//
+
+#ifndef IT2001_2002_L1_ITEM_H
+#define IT2001_2002_L1_ITEM_H
+#include <string>
+using namespace std;
+
+class Item {
+public:
+    Item(string name, int atk);
+
+    Item();
+
+    const string &getName() const;
+
+    void setName(const string &name);
+
+    int getAtk() const;
+
+    void setAtk(int atk);
+
+private:
+    string name;
+    int atk;
+
+};
+
+#endif //IT2001_2002_L1_ITEM_H
